2e8d05ced16b287fff0f793d75c8b812caa597a5
Alcir Junior <junior@cajucomunica.com.br>
2024-02-13 20:51:37 -0300

    feat: :sparkles: add getOrders grpc

--- internal/infra/grpc/service/order_service.go
+++ internal/infra/grpc/service/order_service.go
@@ -10,11 +10,16 @@ import (
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	GetOrderUseCase    usecase.GetOrderUseCase
 }
 
-func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
+func NewOrderService(
+	createOrderUseCase usecase.CreateOrderUseCase,
+	getOrderUseCase usecase.GetOrderUseCase,
+) *OrderService {
 	return &OrderService{
 		CreateOrderUseCase: createOrderUseCase,
+		GetOrderUseCase:    getOrderUseCase,
 	}
 }
 
@@ -34,4 +39,21 @@ func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderReques
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
 	}, nil
+}
+
+func (s *OrderService) GetOrderList(ctx context.Context, in *pb.Blank) (*pb.OrderList, error) {
+	output, err := s.GetOrderUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+	var orders []*pb.Order
+	for _, o := range output {
+		orders = append(orders, &pb.Order{
+			Id:         o.ID,
+			Price:      float32(o.Price),
+			Tax:        float32(o.Tax),
+			FinalPrice: float32(o.FinalPrice),
+		})
+	}
+	return &pb.OrderList{Orders: orders}, nil
 }
